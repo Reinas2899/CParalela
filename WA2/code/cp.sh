d0a3831d2e061571242a3cddbd9fd00de56667e7
axcm19 <afonsoxcm@gmail.com>
2023-11-21 18:47:25 +0000

    script runpar.sh

--- WA2/code/cp.sh
+++ WA2/code/cp.sh
@@ -3,8 +3,11 @@
 #SBATCH --ntasks=40
 #SBATCH --partition=cpar
 
-for i in 2 4 6 16 32
+for i in 2 4 8 16 32
 do
-    export OMP_NUM_THREADS=$i
-    perf stat -e L1-dcache-load-misses -M cpi ./MDpar.exe < inputdata.txt
+        echo "###########################################################################"
+        echo "How many threads? $i"
+        echo "###########################################################################"
+        export OMP_NUM_THREADS=$i
+        perf stat -e L1-dcache-load-misses -M cpi ./MDpar.exe < inputdata.txt
 done
